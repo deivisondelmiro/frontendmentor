ba7d03ca1a8c4f30f7afc94a8b575dbdf92bd314
Deivison Delmiro <deivisondelmirodasilva@gmail.com>
2023-02-24 02:13:38 -0300

    Projeto PING finalizado

---
+++ iniciante/14-ping-coming-soon-page/js/script.js
@@ -0,0 +1,43 @@
+const form = document.querySelector('#form')
+const spanError = document.querySelector('.error')
+const button = form.elements[1]
+form.addEventListener('change', validityInput)
+button.addEventListener('click', validityInputClick)
+
+function validityInput(event) {
+  console.log(event)
+  const target = event.target
+
+  if(!target.checkValidity()) {
+    target.classList.add('errorActive')
+    spanErrorAdd(event)
+  } else {
+    target.classList.remove('errorActive')
+    spanErroRemove()
+  }
+}
+
+function validityInputClick(event) {
+  const input = form.elements[0]
+  event.preventDefault()
+
+  if(input.value.length === 0) {
+    input.classList.add('errorActive')
+    spanError.classList.add('active')
+    spanError.innerText = input.validationMessage
+  } else {
+    input.classList.remove('errorActive')
+    spanErroRemove()
+  }
+}
+
+function spanErrorAdd(event) {
+  const target = event.target
+  spanError.classList.add('active')
+  spanError.innerText = target.validationMessage
+}
+
+function spanErroRemove() {
+  spanError.innerText = ''
+  spanError.classList.remove('active')
+}
