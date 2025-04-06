e994690d6c5312de102a972f6df2fd589bbf1be7
Deivison Delmiro <deivisondelmiro@gmail.com>
2025-04-05 21:01:07 -0300

    Feat: fechamento ao do menu, clicando na parte de fora do menu

--- junior/01-news-homepage-main/js/script.js
+++ junior/01-news-homepage-main/js/script.js
@@ -1,37 +1,43 @@
 const buttonMenu = document.querySelector('.menu-button')
-
-function showMenu() {
-  function activeButtonMenu() {
-    const imgMenu = document.querySelector('.img-menu')
-    buttonMenu.classList.toggle('active')
-
-    const buttonActive = buttonMenu.classList.contains('active')
-
-    if (buttonActive) {
-      imgMenu.setAttribute('src', './assets/images/icon-menu-close.svg')
-      imgMenu.style.position = 'fixed'
-      imgMenu.style.right = '20px'
-      imgMenu.style.top = '38px'
-    } else {
-      imgMenu.setAttribute('src', './assets/images/icon-menu.svg')
-      imgMenu.style.position = 'initial'
-    }
-  }
-  activeButtonMenu()
-
-  function activeMenu() {
-    const navMenu = document.querySelector('.nav-menu')
-    navMenu.classList.toggle('active')
-
-    return true
-  }
-  activeMenu()
-
-  function desfoqueSideMenu() {
-    const desfoquePage = document.querySelector('.desfoque-page')
-    desfoquePage.classList.toggle('active')
-  }
-  desfoqueSideMenu()
-}
+const blurMenuPage = document.querySelector('.desfoque-page')
+const main = document.querySelector('main')
 
 buttonMenu.addEventListener('click', showMenu)
+blurMenuPage.addEventListener('click', closeMenu)
+
+function showMenu() {
+  activeMenu()
+  showButtonMenu()
+  blurSideMenu()
+}
+
+function activeMenu() {
+  const navMenu = document.querySelector('.nav-menu')
+  navMenu.classList.toggle('active')
+  return true
+}
+
+function closeMenu() {
+  showMenu()
+}
+
+function showButtonMenu() {
+  const imgMenu = document.querySelector('.img-menu')
+  buttonMenu.classList.toggle('active')
+
+  const buttonActive = buttonMenu.classList.contains('active')
+
+  if (buttonActive) {
+    imgMenu.setAttribute('src', './assets/images/icon-menu-close.svg')
+    imgMenu.style.position = 'fixed'
+    imgMenu.style.right = '20px'
+    imgMenu.style.top = '38px'
+  } else {
+    imgMenu.setAttribute('src', './assets/images/icon-menu.svg')
+    imgMenu.style.position = 'initial'
+  }
+}
+
+function blurSideMenu() {
+  blurMenuPage.classList.toggle('active')
+}
